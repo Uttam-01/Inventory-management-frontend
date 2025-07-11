87131238b7a4f75e7487e03aabf59a1baf08e58c
Uttam-01 <uttam95081@gmail.com>
2025-07-11 19:08:27 +0530

    fix-bug in build

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import StoreManagerDashboard from "@/components/layout/StoreManagerDashboard";
 import RoleProtected from "@/components/RoleProtection";
-import { useAuthStore } from "@/lib/store/useAuthStore";
 export default function () {
-  const role = useAuthStore((state) => state.role);
-  console.log("Stored role is:", role);
+
   return (
     <RoleProtected allowedRoles={["SUPER_ADMIN", "MANAGER"]}>
       <StoreManagerDashboard />
